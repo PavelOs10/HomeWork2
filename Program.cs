faee9d1a8838ae33d00f8c4c75594a353b5269ee
Pavel Osipov <osiov25@gmail.com>
2024-04-28 20:39:26 +0300

    Добавил метод EnterStringsToArray

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, 
+// либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+
+string[] EnterStringsToArray()
+{
+    Console.WriteLine("Введите количество строк: ");
+    int count = int.Parse(Console.ReadLine());
+
+    string[] workArray = new string[count];
+
+    Console.WriteLine($"Введите {count} строк(у) (после ввода каждой нажмите ENTER):");
+    for (int i = 0; i < count; i++)
+    {
+        workArray[i] = Console.ReadLine();
+    }
+
+    return workArray;
+}
